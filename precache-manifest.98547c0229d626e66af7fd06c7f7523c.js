d9082bd7319a932943f552fdaa154598d1a81877
Mohammad <its_mh@outlook.com>
2020-08-19 08:40:01 -0500

    Updates

---
+++ precache-manifest.98547c0229d626e66af7fd06c7f7523c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b876b7de88b6d07234831a8f52475e6",
+    "url": "/frontend-capstone/index.html"
+  },
+  {
+    "revision": "d0a06481fb3eb8277b64",
+    "url": "/frontend-capstone/static/css/main.5ea3a38d.chunk.css"
+  },
+  {
+    "revision": "d64d74fd62b5d4a48428",
+    "url": "/frontend-capstone/static/js/2.01a4bda3.chunk.js"
+  },
+  {
+    "revision": "f8c1e9d776bb6ca111f1cd3f739e502f",
+    "url": "/frontend-capstone/static/js/2.01a4bda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a06481fb3eb8277b64",
+    "url": "/frontend-capstone/static/js/main.868781e9.chunk.js"
+  },
+  {
+    "revision": "c933f2c0fbeaa35bc86f",
+    "url": "/frontend-capstone/static/js/runtime-main.aa0bfbd2.js"
+  }
+]);
